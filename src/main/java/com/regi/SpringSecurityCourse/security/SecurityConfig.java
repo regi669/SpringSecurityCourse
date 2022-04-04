6ea7e150f5fb0595e75d8f2d913a75b35f07e47d
Szymon <regicsmaster@gmail.com>
2022-04-04 12:35:28 +0200

    Basic auth with JdbcUserDetailsManager

--- src/main/java/com/regi/SpringSecurityCourse/security/SecurityConfig.java
+++ src/main/java/com/regi/SpringSecurityCourse/security/SecurityConfig.java
@@ -8,9 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -30,12 +34,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
     }
+
     /*
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("admin").password("1234").authorities("admin")
                 .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
-    }*/
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -43,6 +48,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails user = User.withUsername("admin").password("12345").authorities("admin").build();
         userDetailsService.createUser(user);
         auth.userDetailsService(userDetailsService);
+    }*/
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource); //In order to use this we need to create users and authorities tables in database
     }
 
     @Bean
