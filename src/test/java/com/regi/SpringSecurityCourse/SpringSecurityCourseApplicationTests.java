e634657e5d684521717fe8c76886fb0d26fb4f10
Szymon <regicsmaster@gmail.com>
2022-04-04 10:33:46 +0200

    Setup for the course

---
+++ src/test/java/com/regi/SpringSecurityCourse/SpringSecurityCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regi.SpringSecurityCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
