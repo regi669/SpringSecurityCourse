19f2939ac2889df8d1a8e1598f7165af74f514af
Szymon <regicsmaster@gmail.com>
2022-04-04 14:08:32 +0200

    Implemented BCrypt password encoder

--- src/main/java/com/regi/SpringSecurityCourse/security/SecurityConfig.java
+++ src/main/java/com/regi/SpringSecurityCourse/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -55,10 +56,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new JdbcUserDetailsManager(dataSource); //In order to use this we need to create users and authorities tables in database
     }*/
 
-
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
 }
